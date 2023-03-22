49559cefcf5386e66c96fda9eb63c4bf540ea612
Sri Panyam <sri.panyam@gmail.com>
2023-03-21 21:01:07 -0700

    Initial commit

---
+++ data/projectsData.ts
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Notations',
+    description: `A modern and advanced viewer/editor for Carnatic music notation`,
+    imgSrc: '/static/images/notations.jpeg',
+    href: 'https://notations.us',
+  },
+  {
+    title: 'TLEX',
+    description: `A lexical analyzer written in pure TypeScript aiming towards incremental lexing`,
+    imgSrc: '/static/images/lexical-analyzer.png',
+    href: 'https://tlexer.appspot.com/tutorial/',
+  },
+]
+
+export default projectsData
