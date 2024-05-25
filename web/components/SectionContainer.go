1db415e827a383b54d3a61d2307b60be23ebd6e1
Sri Panyam <sri.panyam@gmail.com>
2024-05-25 15:30:57 -0700

    No need for components right now - they are in s3 for now as default ones

--- web/components/SectionContainer.go
+++
@@ -1,9 +0,0 @@
-package components
-
-type SectionContainer struct {
-	BaseView
-}
-
-func (v *SectionContainer) RenderResponse() error {
-	return v.Ctx.Template.ExecuteTemplate(v.Ctx.Writer, "SectionContainer.html", v)
-}
--- web/components/TestPage.go
+++
@@ -1,9 +0,0 @@
-package components
-
-type TestPage struct {
-	BaseView
-}
-
-func (v *TestPage) RenderResponse() error {
-	return v.Ctx.Template.ExecuteTemplate(v.Ctx.Writer, "TestPage.html", v)
-}
